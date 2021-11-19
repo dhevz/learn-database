b198d324a29f8a2185c7ca62ed8cd56aedb56ff9
dhevz <66546628+dhevz@users.noreply.github.com>
2021-11-19 12:15:49 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,24 @@ use App\Http\Controllers\BookController;
 |
 */
 
-Route::get('/', [BookController::class, 'index']);
-Route::get('/select', [BookController::class, 'select']);
+Route::get('/cek-object', [BookController::class, 'cekObject']);
 Route::get('/insert', [BookController::class, 'insert']);
-Route::get('/insert-timestamp', [BookController::class, 'insertTimestamp']);
-Route::get('/insert-prepared', [BookController::class, 'insertPrepared']);
-Route::get('/insert-binding', [BookController::class, 'insertBinding']);
+Route::get('/mass-assign1', [BookController::class, 'massAssignment1']);
+Route::get('/mass-assign2', [BookController::class, 'massAssignment2']);
 Route::get('/update', [BookController::class, 'update']);
+Route::get('/update-where', [BookController::class, 'updateWhere']);
+Route::get('/mass-update', [BookController::class, 'massUpdate']);
 Route::get('/delete', [BookController::class, 'delete']);
-Route::get('/statement', [BookController::class, 'statement']);
+Route::get('/destroy', [BookController::class, 'destroy']);
+Route::get('/mass-delete', [BookController::class, 'massDelete']);
+Route::get('/view-all', [BookController::class, 'viewAll']);
+Route::get('/get-where', [BookController::class, 'getWhere']);
+Route::get('/first', [BookController::class, 'first']);
+Route::get('/find', [BookController::class, 'find']);
+Route::get('/latest', [BookController::class, 'latest']);
+Route::get('/limit', [BookController::class, 'limit']);
+Route::get('/skip-take', [BookController::class, 'skipTake']);
+Route::get('/soft-delete', [BookController::class, 'softDelete']);
+Route::get('/with-trashed', [BookController::class, 'withTrashed']);
+Route::get('/restore', [BookController::class, 'restore']);
+Route::get('/force-delete', [BookController::class, 'forceDelete']);
